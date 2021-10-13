6ae84bad83a30b671febc8f1038f7dd064c8a2ac
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-13 10:56:34 +0530

    function literal sleep go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,8 +22,11 @@ func main() {
 		//fmt.Println(<-c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for l := range c {
+		go func(l string) {
+			time.Sleep(time.Second * 5)
+			checkLink(l, c)
+		}(l)
 	}
 
 }
@@ -31,9 +35,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "down down"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "up up"
+	c <- link
 }
